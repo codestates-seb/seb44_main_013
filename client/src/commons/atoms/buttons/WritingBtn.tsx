c40a083fb6972178ea2dadc72c8281447e259689
Emma-Hyejin <cjd191058@naver.com>
2023-07-03 21:17:28 +0900

    [FRONT]Design: 게시판 페이지의 writingBtn 컴포넌트 생성

---
+++ client/src/commons/atoms/buttons/WritingBtn.tsx
@@ -0,0 +1,9 @@
+export default function WritingBtn () {
+    return(
+        <button className="rounded-lg w-fit px-4 py-1 bg-POINT_COLOR text-BASIC_WHITE
+                           text-[8px] hover:bg-HOVER_COLOR cursor-pointer"
+        >
+            Writing
+        </button>
+    )
+}
