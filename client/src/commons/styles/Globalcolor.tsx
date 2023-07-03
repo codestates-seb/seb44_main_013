35122ef3192e1988f2e46fe2baa14ecf4063d2f2
Emma-Hyejin <cjd191058@naver.com>
2023-07-03 14:26:37 +0900

    [FRONT] Design : global style 선정

---
+++ client/src/commons/styles/Globalcolor.tsx
@@ -0,0 +1,10 @@
+//배경 색 
+export const BASIC_BLACK = '#161616';
+//검정 글자 색
+export const BASIC_WHITE = '#ffff';
+//부분 회색
+export const BASIC_GRAY = '#A09F9F';
+//기본 포인트 색상
+export const POINT_COLOR = '#8580E1';
+//포인트 hover 색상
+export const HOVER_COLOR = '#6A66B4';
