5bf6c4f26093c989e70bc1fe8f848345d8198c26
yeon0226 <w01086248649@gmail.com>
2023-07-03 20:53:16 +0900

    [styled] 헤더 스타일 수정 로고를 컴포넌트로 수정하면서 헤더부분을 수정했습니다.

--- client/src/commons/atoms/header/Header.tsx
+++ client/src/commons/atoms/header/Header.tsx
@@ -1,23 +1,15 @@
 import Search from '@/components/search/Search';
-import logo from '../../../assets/logo.png';
-import {
-  CLink,
-  HeaderContainer,
-  ItemContainer,
-  LogoContainer,
-  LogoImg,
-} from './Header.styled';
+import { CLink, HeaderContainer, ItemContainer } from './Header.styled';
 import LoginBtn from '../buttons/login/LoginBtn';
 import UserImg from '../user/UserImg';
 import { useState } from 'react';
+import Logo from '../logo/Logo';
 
 export default function Header() {
   const [isLogin, setIsLogin] = useState(false);
   return (
     <HeaderContainer>
-      <LogoContainer>
-        <LogoImg src={logo} alt="Logo" />
-      </LogoContainer>
+      <Logo />
       <Search />
       <ItemContainer>
         <CLink>community</CLink>
