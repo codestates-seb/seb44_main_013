c6417bc1e432164c953a31b654516fbb6d7a2202
yeon0226 <w01086248649@gmail.com>
2023-07-04 00:19:29 +0900

    [styled] 카테고리 navbar 수정 메인페이지 추합을 위해 스타일을 가운데 정렬로 수정하였습니다.

--- client/src/commons/styles/Containers.styled.tsx
+++ client/src/commons/styles/Containers.styled.tsx
@@ -2,5 +2,8 @@ import { styled } from 'styled-components';
 
 export const FlexContainer = styled.div<{ gap: number }>`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
   gap: ${(props) => props.gap}px;
 `;
