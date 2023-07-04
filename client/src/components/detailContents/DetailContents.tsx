f9fd394e891feb733d8925e9daa4098035669097
HyoJeong-Park <parkhj3003@gmail.com>
2023-07-04 15:43:37 +0900

    feat: Detail Contents form 구현

---
+++ client/src/components/detailContents/DetailContents.tsx
@@ -0,0 +1,13 @@
+import { DetailCntContainer } from './DetailContents.styled';
+
+export default function DetailContents () {
+  return (
+    <DetailCntContainer>
+      <h1>Title</h1>
+      <hr />
+      <p>Contents</p>
+      <hr />
+    </DetailCntContainer>
+  );
+};
+
