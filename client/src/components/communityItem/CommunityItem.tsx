9f12fc374b013576706a3b9c265556a2ddad5a7d
yeon0226 <w01086248649@gmail.com>
2023-07-04 18:43:57 +0900

    게시판 페이지 추합1

--- client/src/components/communityItem/CommunityItem.tsx
+++ client/src/components/communityItem/CommunityItem.tsx
@@ -1,13 +1,14 @@
-import { CommunityItemContainer } from "./CommunityItem.styled";
-import Views from "../views/Views";
+import { CommunityItemContainer } from './CommunityItem.styled';
+import Views from '../views/Views';
+import Image from '@/commons/atoms/Image';
 
-export default function CommunityItem () {
+export default function CommunityItem() {
   return (
     <CommunityItemContainer>
+      <Image />
       <h2>Title</h2>
       <p>contents</p>
       <Views />
     </CommunityItemContainer>
   );
-};
-
+}
--- client/src/pages/community/Community.tsx
+++ client/src/pages/community/Community.tsx
@@ -1,6 +1,8 @@
 import CHeader from '@/commons/atoms/header/CHeader';
 import Search from '@/components/search/Search';
 import { SearchContainer } from './Community.styled';
+import CommunityItem from '@/components/communityItem/CommunityItem';
+import Footer from '@/commons/atoms/footer/Footer';
 
 export default function Community() {
   return (
@@ -9,6 +11,8 @@ export default function Community() {
       <SearchContainer>
         <Search />
       </SearchContainer>
+      <CommunityItem />
+      <Footer />
     </>
   );
 }
