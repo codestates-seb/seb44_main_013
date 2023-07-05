b2b7c6ef08240b03499dcee5dd493e998a8517ac
yeon0226 <w01086248649@gmail.com>
2023-07-05 10:28:36 +0900

    [Add] 회원가입 페이지 구현 회원가입 페이지 배경없이 일단 구조적인 부분만 구현했습니다.

--- client/src/pages/signup/SignUp.styled.tsx
+++ client/src/pages/signup/SignUp.styled.tsx
@@ -1,19 +1,67 @@
 import tw from 'tailwind-styled-components';
 
 export const TextContainer = tw.div`
-  w-1/2
+  w-full
+  md:w-1/2
 `;
+
 export const H1 = tw.div`
+  text-3xl
+  md:text-7xl	
   font-extrabold	
-  text-7xl	
 `;
 
 export const H3 = tw.div`
-  mt-10
-  text-xl
+  mt-4
+  md:mt-10
+  text-sm
+  md:text-xl
 `;
 
 export const SignBtn = tw.button`
+  w-16
+  h-16
+  md:w-32
+  md:h-32
   bg-slate-200
-  mt-6
+  transition-all
+  duration-200
+  ease-in-out
+  hover:bg-slate-300
+  hover:scale-110
+`;
+
+export const ButtonContainer = tw.div`
+  flex
+  justify-between
+  space-x-16
+  width[100%]
+  max-width[24rem]
+  mt-10
+  mx-auto
+`;
+
+export const ContentBox = tw.div`
+max-w-lg
+h-auto
+border-2
+border-gray-200
+rounded-3xl
+flex 
+flex-col
+items-center
+justify-center
+p-10
+text-gray-700
+font-normal
+space-y-4
+mx-auto
+`;
+
+export const Divider = tw.div`
+  hidden
+  md:block
+  border-gray-200
+  border-r-2
+  h-1/2
 `;
--- client/src/pages/signup/SignUp.tsx
+++ client/src/pages/signup/SignUp.tsx
@@ -1,17 +1,57 @@
-import { H1, H3, SignBtn, TextContainer } from './SignUp.styled';
+import { BsArrowReturnLeft } from 'react-icons/bs';
+import {
+  H1,
+  H3,
+  SignBtn,
+  TextContainer,
+  ButtonContainer,
+  ContentBox,
+  Divider,
+} from './SignUp.styled';
+import { BackButton } from '@/commons/atoms/buttons/Button.styled';
 
 export default function SignUp() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <TextContainer>
-        <H1>Choose your account.</H1>
-        <H3>
-          Experience the artistry and ingenuity of diverse portfolios, curated
-          for web
-        </H3>
-      </TextContainer>
-      <SignBtn>Client</SignBtn>
-      <SignBtn>Partner</SignBtn>
+    <div className="flex justify-around items-center h-screen">
+      <BackButton>
+        <BsArrowReturnLeft size={30} />
+      </BackButton>
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <TextContainer>
+          <H1>Choose your account.</H1>
+          <H3>
+            Experience the artistry and ingenuity of diverse portfolios, curated
+            for web
+          </H3>
+        </TextContainer>
+        <ButtonContainer>
+          <SignBtn>Client</SignBtn>
+          <SignBtn>Partner</SignBtn>
+        </ButtonContainer>
+      </div>
+      <Divider />
+      <div className="flex-1">
+        <ContentBox>
+          <div>Portfolly와 함께 최고의 파트너를 만나보세요.</div>
+          <div>
+            저희 플랫폼은 창의적인 아티스트들과 프로젝트를 공유하고 싶은
+            열정적인 클라이언트를 연결하는 곳입니다.
+          </div>
+          <div>
+            우리의 목표는 효과적으로 커뮤니케이션하고, 인상적인 포트폴리오를
+            탐색하고, 마침내 꿈의 파트너를 찾는 것을 돕는 것입니다.
+          </div>
+          <div>
+            클라이언트로서, 당신은 다양한 아티스트의 작품을 즐길 수 있고, 자신의
+            프로젝트에 완벽한 파트너를 찾을 수 있습니다.
+          </div>
+          <div>
+            아티스트로서, 당신은 전세계의 클라이언트들과 협력하여 놀라운
+            프로젝트를 실현시킬 수 있습니다.
+          </div>
+          <div>함께 이 창의적인 여행을 시작해 보세요!</div>
+        </ContentBox>
+      </div>
     </div>
   );
 }
