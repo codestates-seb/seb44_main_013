aacc6375ba3691325769c24c9c422204448791a7
yeon0226 <w01086248649@gmail.com>
2023-07-05 11:07:30 +0900

    [styled] 로그인 페이지에 버튼 추가 로그인 페이지에 뒤로가기 버튼을 추가했습니다.

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,31 +1,48 @@
-import LoginGoogleForm from "@/components/login/LoginGoogleForm"
-import { LoginSection, LoginWrapper, TitleSection, ContentSection, MiddleLine, MiddleWrapper, WriteSection, HorizonLine } from "./Login.styled"
-import BackText from "@/commons/atoms/backText/BackText"
+import LoginGoogleForm from '@/components/login/LoginGoogleForm';
+import {
+  LoginSection,
+  LoginWrapper,
+  TitleSection,
+  ContentSection,
+  MiddleLine,
+  MiddleWrapper,
+  WriteSection,
+  HorizonLine,
+} from './Login.styled';
+import BackText from '@/commons/atoms/backText/BackText';
+import { BackButton } from '@/commons/atoms/buttons/Button.styled';
+import { BsArrowReturnLeft } from 'react-icons/bs';
 
 export default function Login() {
-    return(
-        <LoginWrapper>
-            <BackText>Portfolly</BackText>
-            <LoginSection>
-                <TitleSection>
-                    Discover artworks <br/> that tell stories, <br/> not just simple pictures.
-                </TitleSection>
-                <ContentSection>
-                    Experience the artistry and ingenuity of diverse portfolios, <br/>
-                    curated for web, app, and design enthusiasts.
-                </ContentSection>
-                <LoginGoogleForm>Log in with Google</LoginGoogleForm>
-                <MiddleWrapper>
-                    <MiddleLine/>&nbsp; or &nbsp;<MiddleLine/>
-                </MiddleWrapper>
-                <LoginGoogleForm>Sign up with Google</LoginGoogleForm>
-            </LoginSection>
+  return (
+    <LoginWrapper>
+      <BackText>Portfolly</BackText>
+      <BackButton>
+        <BsArrowReturnLeft size={30} />
+      </BackButton>
+      <LoginSection>
+        <TitleSection>
+          Discover artworks <br /> that tell stories, <br /> not just simple
+          pictures.
+        </TitleSection>
+        <ContentSection>
+          Experience the artistry and ingenuity of diverse portfolios, <br />
+          curated for web, app, and design enthusiasts.
+        </ContentSection>
+        <LoginGoogleForm>Log in with Google</LoginGoogleForm>
+        <MiddleWrapper>
+          <MiddleLine />
+          &nbsp; or &nbsp;
+          <MiddleLine />
+        </MiddleWrapper>
+        <LoginGoogleForm>Sign up with Google</LoginGoogleForm>
+      </LoginSection>
 
-            <HorizonLine/>
+      <HorizonLine />
 
-            <LoginSection>
-                <WriteSection/>
-            </LoginSection>
-        </LoginWrapper>
-    )
+      <LoginSection>
+        <WriteSection />
+      </LoginSection>
+    </LoginWrapper>
+  );
 }
