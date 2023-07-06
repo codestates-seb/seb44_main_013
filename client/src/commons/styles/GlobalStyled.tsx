e55152fd5833f0bf90ca6b350c39bb5085afa90c
yeon0226 <w01086248649@gmail.com>
2023-07-07 00:36:18 +0900

    [styled] 배경 이미지 삽입

--- client/src/commons/styles/GlobalStyled.tsx
+++ client/src/commons/styles/GlobalStyled.tsx
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  body,
-  html {
+   * {
     margin: 0;
     padding: 0;
-    width: 100%;
-    height: 100%;
+    box-sizing: border-box;
   }
-  `;
+  html, body, #root {
+    height: 100%;
+    width: 100%;
+    overflow-x: hidden;
+  }
+`;
