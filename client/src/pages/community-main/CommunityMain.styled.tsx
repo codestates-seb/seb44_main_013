23801cec58e643fdbba51610656eb2fd24b6a869
yeon0226 <w01086248649@gmail.com>
2023-07-07 14:49:11 +0900

    [styled] 게시판 메인 페이지 게시판 메인 페이지의 정렬방식을 수정하였습니다.

--- client/src/pages/community-main/CommunityMain.styled.tsx
+++ client/src/pages/community-main/CommunityMain.styled.tsx
@@ -1,13 +1,14 @@
 import tw from 'twin.macro';
 import { styled } from 'styled-components';
 import communitymainimg from '../../assets/communitymainimg.png';
+import WritingBtn from '@/commons/atoms/buttons/writing/writingBtn';
 
 export const CommunityWrapper = styled.div`
   ${tw`
     w-screen h-full flex flex-col justify-between
     pb-20 bg-center bg-no-repeat bg-cover px-14
   `}
-  /* background-image: url(${communitymainimg}) */
+  background-image: url(${communitymainimg})
 `;
 
 export const SearchContainer = tw.div`
@@ -25,9 +26,15 @@ export const ItemWrapper = tw.div`
   items-end
 `;
 
+export const StyledWritingBtn = styled(WritingBtn)`
+  ${tw`
+    mr-4
+  `}
+`;
+
 export const ListsWrapper = tw.div`
   flex
   flex-wrap
-  justify-between
+  justify-evenly
   gap-4
 `;
