0be896b32e70bdb68c65cce1b48266b77b33f52d
Kim-DaHam <times7749@naver.com>
2023-07-10 15:59:44 +0900

    Fix: LikeBtn & useLikeBtn 두 개의 PropsType을 동일화한다.

--- client/src/commons/atoms/buttons/LikeBtn.tsx
+++ client/src/commons/atoms/buttons/LikeBtn.tsx
@@ -3,22 +3,22 @@ import { SmallText } from '../Typography';
 import { FlexWrapper } from '@/commons/styles/Containers.styled';
 import useLikeBtn from '@/hooks/useLikeBtn';
 
-interface LikeBtnProps {
+export interface LikeBtnProps {
   portfolio_id: number;
-  lastestLikes: number;
-  nowIsLike: boolean;
+  initialLikes: number;
+  initialIsLiked: boolean;
 }
 
-export default function LikeBtn({ portfolio_id, lastestLikes, nowIsLike }: LikeBtnProps) {
+export default function LikeBtn({ portfolio_id, initialLikes, initialIsLiked }: LikeBtnProps) {
   const [likes, color, onClick] = useLikeBtn({
     portfolio_id: portfolio_id,
-    initailLikes: lastestLikes,
-    initialIsLike: nowIsLike,
+    initialLikes: initialLikes,
+    initialIsLiked: initialIsLiked,
   });
   return (
     <FlexWrapper gap={10}>
       <FaHeart color={color} size={25} className='cursor-pointer' onClick={onClick} />
-      <SmallText color='white'>{likes ? likes : lastestLikes}</SmallText>
+      <SmallText color='white'>{likes}</SmallText>
     </FlexWrapper>
   );
 }
