5c7fd2c67984aece57082bf4ffd7973df6031f4f
HyoJeong-Park <parkhj3003@gmail.com>
2023-07-12 09:38:48 +0900

    fix: 검색 컴포넌트 수정

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -4,8 +4,8 @@ export default function Search({setSearchValue, setEnterPress}: any) {
 
   // const [searchValue, setSearchValue] = useState('');
   // 위의 상태를 검색창이 있는 곳에 두고 setSearchValue를 Search 컴포넌트로 내려주었어요.
-  // 여기서 input 의 value를 setSearchValue로 넣어서 검색창 있는 곳에서 value 값을 받았습니다! - 효정
-  // 타입은 임시로 any로 두었어요!
+  // 여기서 input 의 value를 setSearchValue로 넣어서 검색창 있는 곳에서 value 값을 받았습니다!
+  // 타입은 임시로 any로 두었어요! - 효정
 
   // const [enterPress, setEnterPress] = useState(false);
   // input에서 엔터키 쳐야 적용되게끔 하는 상태 설정해두었어요!
