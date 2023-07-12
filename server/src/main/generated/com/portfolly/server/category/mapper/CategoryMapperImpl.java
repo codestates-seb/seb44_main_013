3ab0e49daa786829b39934af66080a458122e96c
sstar914 <saesbyol153@gmail.com>
2023-07-12 20:43:51 +0900

    pull 받기 용 임시 커밋

---
+++ server/src/main/generated/com/portfolly/server/category/mapper/CategoryMapperImpl.java
@@ -0,0 +1,11 @@
+package com.portfolly.server.category.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-07-11T16:11:22+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+public class CategoryMapperImpl implements CategoryMapper {
+}
