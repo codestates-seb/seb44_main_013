9b96aa59d489620f19cc5cbc85acfbf8483092f7
HyoJeong-Park <parkhj3003@gmail.com>
2023-07-12 09:24:13 +0900

    fix: 게시판 메인  수정

--- client/src/pages/community-main/CommunityMain.tsx
+++ client/src/pages/community-main/CommunityMain.tsx
@@ -29,32 +29,7 @@ export default function CommunityMain() {
     axiosCommu();
   }, []);
 
-  const data = [
-    {
-      board_id: 1,
-      title: "박효정씨는 아침 요청입니다.",
-      content: "매일 아침마다 효정씨는 모두에게 아침 인사를 해줍니다. 아주 성실한 친구죠.",
-      view: 208,
-      division: "recruitment",
-      name: "phy",
-      created_at: "2023-06-21T17:34:51.3395597",
-      modifiedAt: "2023-06-21T17:34:51.3395597",
-      member_id: 1,
-      status: true
-    },
-    {
-      board_id: 2,
-      title: "위정연씨는 칭찬 스티커를 줍니다.",
-      content: "잘 하는 사람만 정연씨의 칭찬 스티커를 받을 수 있죠.",
-      view: 200,
-      division: "recruitment",
-      name: "yjy",
-      created_at: "2023-05-21T17:34:51.3395597",
-      modifiedAt: "2023-05-21T17:34:51.3395597",
-      member_id: 2,
-      status: true
-    }
-  ]
+  
   // 검색 - 07.11 효정
   const [searchValue, setSearchValue] = useState('');
   const [searchArr, setSearchArr] = useState([] as any);
@@ -62,7 +37,7 @@ export default function CommunityMain() {
 
   useEffect(() => {
     if(searchValue !== null) {
-      setSearchArr(data.filter((el) => {
+      setSearchArr(datas.filter((el: any) => {
         return el.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) ||
         el.content.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) ||
         el.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
