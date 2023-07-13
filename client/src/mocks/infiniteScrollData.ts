65f6173c94a4a29e99b2f9b6cbfc75e40397aa5e
Emma-Hyejin <cjd191058@naver.com>
2023-07-13 15:20:06 +0900

    Test: 무한 스크롤 더미 데이터 추가

--- client/src/mocks/infiniteScrollData.ts
+++ client/src/mocks/infiniteScrollData.ts
@@ -53,6 +53,421 @@ export const commu: CommuProps[] = [
     modifiedAt: "2023-04-21T17:34:51.3395597",
     memberId: 4,
     status: "POST_ACTIVE"
+  },
+  {
+    id: 5,
+    //유저 프로필 이미지
+    title: "테스트5",
+    content: "테스트 컨탠트5",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 5,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 6,
+    //유저 프로필 이미지
+    title: "테스트6",
+    content: "테스트 컨탠트4",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 6,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 7,
+    //유저 프로필 이미지
+    title: "테스트7",
+    content: "테스트 컨탠트4",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 7,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 8,
+    //유저 프로필 이미지
+    title: "테스트8",
+    content: "테스트 컨탠트8",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 8,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 9,
+    //유저 프로필 이미지
+    title: "테스트9",
+    content: "테스트 컨탠트9",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 9,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 10,
+    //유저 프로필 이미지
+    title: "테스트10",
+    content: "테스트 컨탠트10",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 10,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 11,
+    //유저 프로필 이미지
+    title: "테스트11",
+    content: "테스트 컨탠트11",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 11,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 12,
+    //유저 프로필 이미지
+    title: "테스트12",
+    content: "테스트 컨탠트12",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 13,
+    //유저 프로필 이미지
+    title: "테스트13",
+    content: "테스트 컨탠트13",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 14,
+    //유저 프로필 이미지
+    title: "테스트14",
+    content: "테스트 컨탠트14",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 15,
+    //유저 프로필 이미지
+    title: "테스트15",
+    content: "테스트 컨탠트15",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 16,
+    //유저 프로필 이미지
+    title: "테스트16",
+    content: "테스트 컨탠트16",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 17,
+    //유저 프로필 이미지
+    title: "테스트17",
+    content: "테스트 컨탠트17",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 18,
+    //유저 프로필 이미지
+    title: "테스트18",
+    content: "테스트 컨탠트18",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 19,
+    //유저 프로필 이미지
+    title: "테스트19",
+    content: "테스트 컨탠트19",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 20,
+    //유저 프로필 이미지
+    title: "테스트20",
+    content: "테스트 컨탠트20",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 21,
+    //유저 프로필 이미지
+    title: "테스트21",
+    content: "테스트 컨탠트21",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 22,
+    //유저 프로필 이미지
+    title: "테스트22",
+    content: "테스트 컨탠트22",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 23,
+    //유저 프로필 이미지
+    title: "테스트23",
+    content: "테스트 컨탠트23",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 24,
+    //유저 프로필 이미지
+    title: "테스트24",
+    content: "테스트 컨탠트24",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 25,
+    //유저 프로필 이미지
+    title: "테스트25",
+    content: "테스트 컨탠트25",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 26,
+    //유저 프로필 이미지
+    title: "테스트26",
+    content: "테스트 컨탠트26",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 27,
+    //유저 프로필 이미지
+    title: "테스트27",
+    content: "테스트 컨탠트27",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 28,
+    //유저 프로필 이미지
+    title: "테스트28",
+    content: "테스트 컨탠트28",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 29,
+    //유저 프로필 이미지
+    title: "테스트29",
+    content: "테스트 컨탠트29",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 30,
+    //유저 프로필 이미지
+    title: "테스트30",
+    content: "테스트 컨탠트30",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 31,
+    //유저 프로필 이미지
+    title: "테스트31",
+    content: "테스트 컨탠트31",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 32,
+    //유저 프로필 이미지
+    title: "테스트32",
+    content: "테스트 컨탠트32",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 33,
+    //유저 프로필 이미지
+    title: "테스트33",
+    content: "테스트 컨탠트33",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 34,
+    //유저 프로필 이미지
+    title: "테스트34",
+    content: "테스트 컨탠트34",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 35,
+    //유저 프로필 이미지
+    title: "테스트35",
+    content: "테스트 컨탠트35",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
+  },
+  {
+    id: 36,
+    //유저 프로필 이미지
+    title: "테스트36",
+    content: "테스트 컨탠트36",
+    view: 150,
+    division: "COOPERATION",
+    name: "testUser",
+    created_at: "2023-04-21T17:34:51.3395597",
+    modifiedAt: "2023-04-21T17:34:51.3395597",
+    memberId: 4,
+    status: "POST_ACTIVE"
   }
-  ///
 ];
