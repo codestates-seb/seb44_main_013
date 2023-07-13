64986d0db8001baf0c0cff59de07d3bce27d0ca3
HyoJeong-Park <parkhj3003@gmail.com>
2023-07-13 16:33:17 +0900

    fix: 커뮤니티 아이템 충돌 수정

--- client/src/components/communityItem/CommunityItem.tsx
+++ client/src/components/communityItem/CommunityItem.tsx
@@ -8,17 +8,18 @@ import Views from '../views/Views';
 import UserProfile from '@/commons/molecules/UserProfile';
 
 
-export default function CommunityItem({communityItem}: any) {
+export default function CommunityItem(datas: any) {
   const navigate = useNavigate();
-  const eachData = communityItem;
+  const eachData = datas.datas
 
-  const HandleClick = (event: CommuProps) => {
-    navigate(`/boards/${event.board_id}`, { state: event });
+  const handleLink = (e: CommuProps) => {
+    navigate(`/boards/${e.id}`, { state: e });
+    console.log(e.id);
   }
 
   return (
-    <CommunityItemContainer onClick={() => { HandleClick(eachData) }}>
-      <UserProfile type={'board'} user={{ member_id: eachData.member_id, name: eachData.name, picture: 'https://picsum.photos/200/300' }} />
+    <CommunityItemContainer onClick={() => { handleLink(eachData) }}>
+      <UserProfile type={'board'} user={{ member_id: eachData.memberId, name: eachData.name, picture: 'https://picsum.photos/200/300' }} />
       <h2>{eachData.title}</h2>
       <p>{eachData.content}</p>
       <Views view={eachData.view} />
