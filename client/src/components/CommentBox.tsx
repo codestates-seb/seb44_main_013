c72091042f4cb6e2b3ea46c33d54780c2d59be13
Emma-Hyejin <cjd191058@naver.com>
2023-07-13 13:46:55 +0900

    Refactor: 멘토링 이후 코드 수정

--- client/src/components/CommentBox.tsx
+++ client/src/components/CommentBox.tsx
@@ -1,78 +1,102 @@
 /* 2023-07-05 게시물 상세보기 페이지 댓글 영역 컴포넌트 - 김다함 */
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Card from '@/commons/atoms/Card';
 import Comment from '@/commons/molecules/Comment';
-import { FlexColumnContainer } from '@/commons/styles/Containers.styled';
 import CommentWriteBox from '@/commons/molecules/CommentWriteBox';
+
 import { CommentProps } from '@/types';
 
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { useState } from 'react';
 import { JbWrapper } from '@/pages/community-detail/CommunityDetail.styled';
+import { FlexColumnContainer } from '@/commons/styles/Containers.styled';
+
+import { call } from '@/utils/ApiService';
 
 
-export default function CommentBox( {comment, }:any) {
-    const [isInput, setIsInput ] = useState('');
-    const [ amendComment, setAmendComment ] = useState(comment);
-    const board_id = useParams();
+export default function CommentBox( {comments = [] }:any) { // comment => comments
+    const [ currentComment, setCurrentComment ] = useState('');
+    //is로 시작하면 boolean으로 생각할 수도 있다. (isInput -> currentComment)
+    //Fn+f2
+    const [ amendComment, setAmendComment ] = useState(comments);
+    const [ deleteId, setDeleteId ] = useState(null);
+    const { board_id:boardId } = useParams(); //수정
     
     const handleComment = ( value: string ) => {
-        setIsInput(value);
+        setCurrentComment(value);
     }
 
+    //return문이 없고 async 체크 필요 -> await으로 받는 부분이 없음. (0712)
+    //async - await 
     const saveComment = () => {
+        const addNewComment = () => call(`/comments`, 'POST', {
+            boardId: boardId,
+            content: currentComment,
+        });
         const axiosPost = async () => {
-        try{
-            console.log('POST 성공');
-
-            axios('/comments', {
-            method:'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: JSON.stringify({ 
-                board_id: board_id,
-                content: isInput,
+            return addNewComment()
+            .then((res) => {
+                console.log(res);
             })
-            })
-            //console.log(board_id);
-
-        } catch(err) {
-            console.log('POST실패 ' + err);
-        }
-        };
+            .catch((err) => console.log('댓글 등록 에러' + err))
+        } 
 
         axiosPost();
-        comment.push({
-            comments_id:comment.length + 1,
-            content: isInput,
+        amendComment.push({
+            comments_id:comments.length + 1,
+            content: currentComment,
             name: 'jhj',
             createdAt: "2023-06-21T17:34:51.3395597",
-            modifiedAt:"2023-06-21T17:34:51.3395597"
+            modifiedAt:"2023-06-21T17:34:51.3395597",
+            status: "POST_ACTIVE"
         });
-        setIsInput('');
-        //console.log(comment);
-    }
+        setCurrentComment('');
+        // console.log(comments);
+        // console.log(amendComment);
+    };
+
+    useEffect(() => {
+        //약자는 최대한 쓰지 말자.
+        const index = amendComment.findIndex((element:any) => element.comments_id === deleteId);
+
+        // 첫번째 댓글 지울 때 
+        if( index === 0 ){ // 수정합시당 : 과제 
+            setAmendComment(amendComment.slice(1, amendComment.length));
+        }
+        // 둘이 겹침 
+        if( index > 0 ) {
+            setAmendComment(amendComment.slice(0, index).concat(amendComment.slice(index+1, amendComment.length)));
+        }
+
+        // else if 쓰지말장. - 현직자들이 싫어한당
+        setDeleteId(null);
+        // console.log(newComment);
+        // -1 는 직독이해가 되지 않는다. 명시적 data 사용 ex. null
+    }, [deleteId])
+
+    // console.log(amendComment)
 
     return (
         <Card>
             <JbWrapper >
                 <FlexColumnContainer gap={0} className="overflow-scroll">
+                    {/* prettier amendcomment -> ammendComments  ;; element -> amendComment*/}
                     {
-                        comment.map((e:CommentProps) => {
-                            return (<Comment key={e.comments_id}
-                                            username={e.name}
-                                            content={e.content}
-                                            date={e.createdAt}
-                                            comment={e}
-                                            amendComment={amendComment}
+                        amendComment.map((element:CommentProps) => {
+                            return (<Comment key={element.comments_id}
+                                            username={element.name}
+                                            content={element.content}
+                                            date={element.createdAt}
+                                            comments={element}
+                                            setDeleteId={setDeleteId}
                                             setAmendComment={setAmendComment}
                                     />
                             )
                         })
                     }
                 </FlexColumnContainer>
-                <CommentWriteBox saveComment={saveComment} handleComment={handleComment} isInput={isInput}/>
+                <CommentWriteBox saveComment={saveComment} handleComment={handleComment} isInput={currentComment}/>
             </JbWrapper>
         </Card>
     )
--- client/src/pages/community-detail/CommunityDetail.tsx
+++ client/src/pages/community-detail/CommunityDetail.tsx
@@ -1,56 +1,59 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
 import { call } from '@/utils/ApiService';
 
 import UserProfile from '@/commons/molecules/UserProfile';
 import DetailContents from '@/components/detailContents/DetailContents';
+import Loading from '../../components/loading/Loading'; // components/loding 이동 (page x) 수정 완 
+import CommentBox from '@/components/CommentBox';
+
+import { CommuProps } from '@/types';
+
 import {
   CmDContainer,
   CommentContainer,
   MainContainer,
   PageWrapper,
 } from './CommunityDetail.styled';
-import Loading from '../404/Loading';
-import CommentBox from '@/components/CommentBox';
-import { CommuProps } from '@/types';
 
-export default function CommunityDetail({ handleClick }: any) {
-  const [memberData, setMemberData] = useState<CommuProps>();
-  const { board_id } = useParams();
+export default function CommunityDetail( {handleClick}:any ) {
+  const [ memberData, setMemberData ] = useState<CommuProps | null>(null); 
+  const { board_id:boardId } = useParams(); //_ 상수일 때만 사용 + 전체 대문자 -> 별칭 : boardId로 쓰겠다.
 
-  useEffect(() => {
-    const axiosMember = async () => {
-      return call(`/boards/${board_id}`, 'GET', null)
-        .then((res) => {
-          setMemberData(res[0]);
-        })
-        .catch((err) => console.log('커뮤니티 상세 페이지 예시' + err));
+  useEffect(()=> {
+    const findBoardsById = (id: string) => call(`/boards/${id}`, 'GET', null);
+    const getMember = async () => { //axiosMember(xxx) 이름에서부터 유추 (한글 직독직 -> 영어)
+      return findBoardsById(boardId as string)
+      .then((res) => {
+        setMemberData(res[0]);
+      })
+      .catch((err) => console.log('커뮤니티 상세 페이지 예시' + err)); 
     };
 
-    axiosMember();
-  }, [board_id]);
-
+    getMember();
+  }, [boardId]);
+  // earlyreturn pattern : memberData가 없는 경우 37까지만 실행하고 끝 -> 성능 상승 
+  if(!memberData) return <PageWrapper><Loading/></PageWrapper>
+ // eslint prettier 적용하기 : 과제 (0712)
   return (
     <PageWrapper >
-      {memberData !== undefined ?
-        (<>
-          <UserProfile type={'blackboard'}
-            user={{
-              member_id: memberData.member_id,
-              name: memberData.name,
-              picture: 'https://picsum.photos/200/300'
-            }}
-          />
-          <MainContainer onClick={handleClick}>
-            <CmDContainer>
-              <DetailContents data={memberData} />
-            </CmDContainer>
-            <CommentContainer>
-              <CommentBox comment={memberData.comment} />
-            </CommentContainer>
-          </MainContainer>
-        </>
-        ) : <><Loading /></>}
+      {/* !== 처럼 바로 이해안되는 거는 노노  -> 삼항연산자 삭제 */}
+      {/* member_id change */}
+        <UserProfile type={'blackboard'} 
+          user={{ member_id: memberData.member_id, 
+                  name: memberData.name, 
+                  picture: 'https://picsum.photos/200/300' 
+                }} 
+        />
+        <MainContainer onClick={handleClick}>
+          <CmDContainer>
+            <DetailContents data={memberData}/>
+          </CmDContainer>
+          <CommentContainer>
+            <CommentBox comments={memberData.comments}/>
+          </CommentContainer>
+        </MainContainer>
     </PageWrapper>
   );
 }
