0e12cad5e3146dbc6bb44d2ed75e6d0cd76cbf32
Kim-DaHam <times7749@naver.com>
2023-07-17 13:07:26 +0900

    Fix: CATEGORY_TYPE 상수 타입을 적용한다.

--- client/src/modules/categorySlice.ts
+++ client/src/modules/categorySlice.ts
@@ -1,13 +1,14 @@
 /* 2023-07-12 카테고리 스토어 - 김다함 */
 // CategorySlice => categorySlice (컴포넌트 아니면 camelCase)
 import { createSlice } from '@reduxjs/toolkit';
+
 import { RootState } from '@/modules/index';
-import { CategorySlice } from '@/types';
+import { CATEGORY_TYPE, CategorySlice } from '@/types';
 
 export const SET_CATEGORY = 'SET_CATEGORY' as const;
 export const OPEN_CATEGORY = 'OPEN_CATEGORY' as const;
 
-export const setCategory = (category: string) => ({ type: SET_CATEGORY, category });
+export const setCategory = (category: CATEGORY_TYPE) => ({ type: SET_CATEGORY, category });
 export const openCategory = (isOpened: boolean) => ({ type: OPEN_CATEGORY, isOpened });
 
 const initialState: CategorySlice = {
