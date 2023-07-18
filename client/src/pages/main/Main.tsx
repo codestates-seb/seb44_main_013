1d500ea5b907ff4f837057cf1d6459f99bf19e7b
yeon0226 <w01086248649@gmail.com>
2023-07-18 20:23:07 +0900

    [refactore] 메인페이지 코드 리팩토링

--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -6,78 +6,74 @@ import { call } from '@/utils/apiService';
 import CategoryNavBar from '@/components/navbar/CategoryNavBar';
 import WebItem from '@/components/webItem/WebItem';
 import { WebItemsContainer } from './Main.styled';
-// StyledBackground,
-//import { WholeWrapper } from '@/commons/styles/MainLayout';
-import '../../index.css';
 import AppItem from '@/components/appItem/AppItem';
 import GraphicItem from '@/components/graphicItem/GraphicItem';
 import PhotoItem from '@/components/photoItem/PhotoItem';
 import ThreeDItem from '@/components/threeDitem/ThreeDITem';
 import Search from '@/components/search/Search';
 
+const categoryMap = {
+  웹: 'web',
+  앱: 'app',
+  '3D/애니메이션': '3danimation',
+  그래픽디자인: 'graphicdesign',
+  '사진/영상': 'photo',
+};
+
 export default function Main() {
   const selectedCategory = useSelector(category);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    const getCategoryDatas = async () => {
-      if (selectedCategory === '웹') {
-        return call(`portfolios?category=web`, 'GET', null).then((res) =>
-          setItems(res[0].data)
+    const fetchData = async () => {
+      try {
+        const categoryParam = categoryMap[selectedCategory] || 'web';
+        const res = await call(
+          `portfolios?category=${categoryParam}`,
+          'GET',
+          null
         );
+        setItems(res[0].data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      if (selectedCategory === '앱') {
-        return call(`portfolios?category=app`, 'GET', null).then((res) =>
-          setItems(res[0].data)
-        );
-      }
-      if (selectedCategory === '3D/애니메이션') {
-        return call(`portfolios?category=3danimation`, 'GET', null).then(
-          (res) => setItems(res[0].data)
-        );
-      }
-      if (selectedCategory === '그래픽디자인') {
-        return call(`portfolios?category=graphicdesign`, 'GET', null).then(
-          (res) => setItems(res[0].data)
-        );
-      }
-      if (selectedCategory === '사진/영상') {
-        return call(`portfolios?category=photo`, 'GET', null).then((res) =>
-          setItems(res[0].data)
-        );
-      }
-
-      return await call(`portfolios?category=web`, 'GET', null).then((res) =>
-        setItems(res[0].data)
-      );
     };
 
-    getCategoryDatas();
+    fetchData();
   }, [selectedCategory]);
+
+  const renderItems = () => {
+    switch (selectedCategory) {
+      case '웹':
+        return items.map((element, index) => (
+          <WebItem item={element} key={index} />
+        ));
+      case '앱':
+        return items.map((element, index) => (
+          <AppItem item={element} key={index} />
+        ));
+      case '3D/애니메이션':
+        return items.map((element, index) => (
+          <ThreeDItem item={element} key={index} />
+        ));
+      case '그래픽디자인':
+        return items.map((element, index) => (
+          <GraphicItem item={element} key={index} />
+        ));
+      case '사진/영상':
+        return items.map((element, index) => (
+          <PhotoItem item={element} key={index} />
+        ));
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
       <Search />
       <CategoryNavBar />
-      <WebItemsContainer>
-        {items.map((element, index) => {
-          if (selectedCategory === '웹') {
-            return <WebItem item={element} key={index} />;
-          }
-          if (selectedCategory === '앱') {
-            return <AppItem item={element} key={index} />;
-          }
-          if (selectedCategory === '3D/애니메이션') {
-            return <ThreeDItem item={element} key={index} />;
-          }
-          if (selectedCategory === '그래픽디자인') {
-            return <GraphicItem item={element} key={index} />;
-          }
-          if (selectedCategory === '사진/영상') {
-            return <PhotoItem item={element} key={index} />;
-          }
-        })}
-      </WebItemsContainer>
+      <WebItemsContainer>{renderItems()}</WebItemsContainer>
     </>
   );
 }
