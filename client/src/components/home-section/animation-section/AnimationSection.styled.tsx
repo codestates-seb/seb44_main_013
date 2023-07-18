f8a4c0c789a8854f8e008e8cf1d68a7a9920dc3e
yeon0226 <w01086248649@gmail.com>
2023-07-18 15:15:08 +0900

    [styled] 기선제압 페이지 기선제압 페이지의 제각각인 이미지 크기를 전체비율로 통일시킴

--- client/src/components/home-section/animation-section/AnimationSection.styled.tsx
+++ client/src/components/home-section/animation-section/AnimationSection.styled.tsx
@@ -4,6 +4,11 @@ export const SectionWrapper = styled.div``;
 
 export const ImageWrapper = styled.div`
   position: relative;
+  .image {
+    width: 100%; // 이미지의 너비를 ImageWrapper의 너비에 맞춤
+    height: 100vh; // 이미지의 높이를 뷰포트의 높이에 맞춤
+    object-fit: cover; // 이미지의 비율을 유지하면서, 지정한 너비와 높이에 맞게 이미지를 잘라내거나 확대/축소
+  }
 `;
 
 export const TextOverlay = styled.div`
