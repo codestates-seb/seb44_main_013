702ac165d4fabd80f9a247b4db1ae6ac31cd36fa
HyoJeong-Park <parkhj3003@gmail.com>
2023-07-19 15:46:48 +0900

    fix: 등록, 수정 페이지 코드 수정

--- client/src/pages/community-add/AddCommunity.tsx
+++ client/src/pages/community-add/AddCommunity.tsx
@@ -14,7 +14,6 @@ import {
   TextEditor,
   TitleAdd,
 } from './AddCommunity.styled';
-// import { CommuProps } from '@/types';
 
 export default function AddCommunity() {
   const modules = {
@@ -37,8 +36,6 @@ export default function AddCommunity() {
 
   // 상수로 빼는 방법도 있음!
   
-  // post 요청
-
   const [title, setTitle] = useState("");
   const [post, setPost] = useState("");
   const navigate = useNavigate();
@@ -48,80 +45,85 @@ export default function AddCommunity() {
     setTitle(e.target.value);
   };
   //새 등록인지 수정인지 한 페이지에서 관리 
-  //  post 요청 body
-  const postInformation = {
-    title: title,
-    content: post,
-    division: 'RECRUITMENT',
-  }
-
-  //새 글 작성 함수
-  // const postCommunity = () => {
-  //   axios.post('/api/boards/write', {
-  //       headers: {
-  //         widthCredentials: true,
-  //       },
-  //       body: postInformation,
-  //     }
-  //   )  
-  //   .then((res) => console.log('post 요청 성공 ' + res))
-  //   .catch((err) => console.log(err));
-
-  //   navigate('/boards?division=RECRUITMENT');
+  // post 요청 body
+  // const communityContent = {
+  //   title: title,
+  //   content: post.replace(/<\/?p[^>]*>/g, ''),
   // }
 
-  // patch 요청
-  // 게시글 클릭했을 때의 게시글 number로 api 보내서 get 요청 해 res 받아오기
-  // 받아온 res 제목과 내용에 각각 배치
-  // 배치한 내용에서(원본) 수정한다면 patch요청 보내기
-  // const communityNum = 1; // 임시로
+
+  //새 글 작성 함수
+  const postCommunity = () => {
+    // axios.post('/api/boards/write', {
+    //     headers: {
+    //       "Content-Type": "application/json"
+    //     },
+    //     body: {
+    //       title: title,
+    //       content: post.replace(/<\/?p[^>]*>/g, ''),
+    //       division: 'RECRUITMENT'
+    //       // division 임시
+    //     }
+    //   }
+    // )
+    axios.post('/api/boards/write', {
+        title: title,
+        content: post.replace(/<\/?p[^>]*>/g, ''),
+        division: 'RECRUITMENT'
+        // division 임시
+      })
+        .then((res) => console.log('갔어요! ' + res))
+        .catch((err) => console.log('안갔어요!' + err));
+        navigate('/boards?division=RECRUITMENT');
+  }
+
   const { id: communityNum } = useParams();
+  // console.log(location.pathname);
   //url에서 boardId 받아옵니다. 
   // 최초 랜더링 시 해당 게시글을 reqct-quill에 뿌립니다. 
-  useEffect(() => {
-    if(location.pathname === `/boards/edit/${communityNum}`){
+  // useEffect(() => {
+  //   if(location.pathname === `/boards/edit/${communityNum}`){
+  //     axios.get(`/api/boards/${communityNum}`)
+  //     .then((res) => {
+  //       setTitle(res.data.title);
+  //       setPost(res.data.content);
+  //     })
+  //     .catch((err) => console.log(err));
+  //   }
+
+    // if(location.pathname === `/boards/edit`) {
+    //   axios.post(`/api/boards/write`,  {
+    //     headers: {
+    //       widthCredentials: true,
+    //     },
+    //     body: postInformation,
+    //   })
+    //   .then(() => console.log('새글 등록 성공'))
+    //   .catch((err) => console.log(err))
+
+    //   navigate(`/boards/`)
+    // }
+
+  // }, [])
+  if(location.pathname === `/boards/edit/${communityNum}`) {
+    useEffect(() => {
       axios.get(`/api/boards/${communityNum}`)
       .then((res) => {
         setTitle(res.data.title);
         setPost(res.data.content);
       })
       .catch((err) => console.log(err));
-    }
-
-    if(location.pathname === `/boards/write`) {
-      axios.post(`/api/boards/write`,  {
-        headers: {
-          widthCredentials: true,
-        },
-        body: postInformation,
-      })
-      .then(() => console.log('새글 등록 성공'))
-      .catch((err) => console.log(err))
-
-      navigate(`/boards/`)
-    }
-
-  }, [])
+    }, [])
+  }
 
   const saveNewCommuData = () =>  {
     axios.patch(`/api/boards/edit/${communityNum}`, {
       title: title,
-      content: post
+      content: post.replace(/<\/?p[^>]*>/g, '')
     })
     .then(() => navigate(`/boards/${communityNum}`))
     .catch((err) => console.log(err));
-
-    
   }
-
-  // const changeCommunity = () => {
-  //   axios.patch(`/boards/edit/:${communityNum}`, postInformation)
-  // }  : 임시 주석
-
-  // community 뒤에 게시글 id 넣기
-  // create, patch 를 같은 페이지에서 게시글 number params 여부에 따라
-  // get 넣을지 안 넣을지 선택, value에 조건부 렌더링
-
   return (
     <>
       {/* <CHeader /> */}
@@ -139,9 +141,19 @@ export default function AddCommunity() {
               onChange={(event)=> setPost(event)}
             />
             {/* (value: string, delta: DeltaStatic, source: Sources, editor: ReactQuill.UnprivilegedEditor)  */}
-            <SaveBtnContainer onClick={saveNewCommuData}>
-              <PurpleBtn >Save</PurpleBtn>
-            </SaveBtnContainer>
+            {
+              location.pathname === '/boards/edit' ? 
+              <>
+                <SaveBtnContainer onClick={postCommunity}>
+                <PurpleBtn >Save</PurpleBtn>
+                </SaveBtnContainer>
+              </> :
+              <>
+                <SaveBtnContainer onClick={saveNewCommuData}>
+                <PurpleBtn >Save</PurpleBtn>
+                </SaveBtnContainer>
+              </>
+            }
           </TextEditor>
         </TextEditorContainer>
         <div className='guideContainer'>
