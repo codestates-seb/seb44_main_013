6f3cf2908f68e2edc94fbf84cc6e8757c41e0c44
Kim-DaHam <times7749@naver.com>
2023-07-21 01:36:31 +0900

    Memo: 멘토링 팁 주석을 작성한다.

--- client/src/hooks/useImageHandler.tsx
+++ client/src/hooks/useImageHandler.tsx
@@ -1,7 +1,17 @@
-/* 2023-07-07 좋아요 버튼 커스텀훅 - 김다함 */
+/* 2023-07-14 이미지 핸들러 커스텀훅 - 김다함 */
 import { useCallback } from 'react';
 
 import { call } from '@/utils/apiService';
+
+// 이미지 업로드 순서
+// 1. 파일 탐색기에서 이미지 선택
+// 2. 이미지 받아오면 받아 오자마자 서버로 보냅니다.
+// 3. 이미지 저장 성공 여부, 이미지의 url + id
+// 4. 상태에 이미지 url 업데이트
+// 5. react-quill 에디터에 있는 이미지를 서버꺼랑 바꿔치기
+// 6. 삭제하는 경우에는: 바꿔치기된 것의 id 혹은 url을 포함하여 요청을 서버에 날려주면 됨.
+// 7. 되돌리기를 한다? -> 서버에 삭제표시했던 이미지 다시 가져오셈
+
 
 export default function useImageHandler() {
   const uploadImage = (body: FormData) => call('/pictures', 'POST', body);
