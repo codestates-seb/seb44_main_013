e5cb5ba7451bc186565ccd5f1a953669bba85675
yeon0226 <w01086248649@gmail.com>
2023-07-21 14:23:03 +0900

    [add, styled]기선제압페이지 기선제압 하단 이미지 교체

--- client/src/components/home-section/display-section/DisplaySection.tsx
+++ client/src/components/home-section/display-section/DisplaySection.tsx
@@ -5,39 +5,35 @@ import {
   GalleyWrapper,
   GalleyList,
   PortFollyButtonWrapper,
-
 } from './DisplaySection.styled';
 import { Element } from 'react-scroll';
 
 import PortfollyBtn from '@/commons/atoms/portfolly/PortfollyBtn';
 
-
-import appImg from '@/assets/appImg.png';
-import photoImg from '@/assets/photoImg.png';
-import webImg from '@/assets/WebItem1.png';
-import graphinImg from '@/assets/graphinImg.png';
-import drawImg from '@/assets/PortfolioImage2.png';
+import appImg from '@/assets/photo1.png';
+import photoImg from '@/assets/photo2.png';
+import webImg from '@/assets/photo3.png';
+import graphinImg from '@/assets/photo4.png';
+import drawImg from '@/assets/photo5.png';
 
 const DisplayWrapper = () => {
-
-
   return (
-    <Element name='displaySection'>
+    <Element name="displaySection">
       <SectionWrapper>
-        <PortFollyButtonWrapper data-aos='fade' data-aos-delay='3000'>
-          <PortfollyBtn/>
+        <PortFollyButtonWrapper data-aos="fade" data-aos-delay="3000">
+          <PortfollyBtn />
         </PortFollyButtonWrapper>
-        <div className='IntroduceWrapper' data-aos='fade-up'>
-          <IntroducePortfolly >You can display your own</IntroducePortfolly>
-          <CircleForDisplay className='galleyTop'></CircleForDisplay>
+        <div className="IntroduceWrapper" data-aos="fade-up">
+          <IntroducePortfolly>You can display your own</IntroducePortfolly>
+          <CircleForDisplay className="galleyTop"></CircleForDisplay>
           <GalleyWrapper>
-            <GalleyList src={webImg} alt='webImg' />
-            <GalleyList src={appImg} alt='appImg' />
-            <GalleyList src={photoImg} alt='photoImg' />
-            <GalleyList src={graphinImg} alt='graphinImg' />
-            <GalleyList src={drawImg} alt='drawImg' />
+            <GalleyList src={webImg} alt="webImg" />
+            <GalleyList src={appImg} alt="appImg" />
+            <GalleyList src={photoImg} alt="photoImg" />
+            <GalleyList src={graphinImg} alt="graphinImg" />
+            <GalleyList src={drawImg} alt="drawImg" />
           </GalleyWrapper>
-          <CircleForDisplay className='galleyBottom'></CircleForDisplay>
+          <CircleForDisplay className="galleyBottom"></CircleForDisplay>
           <IntroducePortfolly>in our Portfolly</IntroducePortfolly>
         </div>
       </SectionWrapper>
