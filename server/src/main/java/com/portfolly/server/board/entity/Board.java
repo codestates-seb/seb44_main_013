907839e295abf0869c3999c0ff73fb8b10f6e165
SEB_BE_44_정다희 <125248034+sftm0715@users.noreply.github.com>
2023-07-24 04:44:35 +0900

    fix: board 오류사항수정

--- server/src/main/java/com/portfolly/server/board/entity/Board.java
+++ server/src/main/java/com/portfolly/server/board/entity/Board.java
@@ -1,6 +1,6 @@
 package com.portfolly.server.board.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.portfolly.server.audit.Auditable;
 import com.portfolly.server.comment.dto.CommentDto;
@@ -42,7 +42,7 @@ public class Board extends Auditable {
     @Enumerated(value = EnumType.STRING)
     private Status status = Status.POST_ACTIVE;
 
-    @JsonIgnore
+    @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
