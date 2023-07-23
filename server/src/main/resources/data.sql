a42ffbf250a9cd8b585dee05bb52251be606285b
이희재 <96697031+dlglwo123@users.noreply.github.com>
2023-07-24 01:22:24 +0900

    Update data.sql

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -24,3 +24,5 @@ VALUES ('react'),
 --         'explains explains2','ACTIVE', 'title is title', 0, 1, 1),
 --        ('2023-07-20 17:03:39.017297','content<br><img src="https://portfolly-picture.s3.ap-northeast-2.amazonaws.com/29619c04-a739-4e3e-b1c4-9f8453aa4f40.png"><br>wkfskdhskdy',
 --         'explains explains','ACTIVE', 'title is title', 0, 1, 1);
+INSERT INTO member (name, email, image_url, member_role, location, com_info, job, career, award, skill, refresh_token, member_status)
+VALUES ('John', 'john@example.com', 'http://example.com/image.jpg', 'CLIENT', 'New York', 'Some company info', 'Software Engineer', '5 years', 'Received XYZ award', 'Java, Python, SQL', 'some_refresh_token', 'MEMBER_ACTIVE');
