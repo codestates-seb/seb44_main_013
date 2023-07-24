4cce3f830253eebdda576ace31cfa9dd41b69882
Emma-Hyejin <cjd191058@naver.com>
2023-07-24 09:56:26 +0900

    Feat: nestAxios 기능 수정

--- client/src/utils/axiosIntercept.tsx
+++ client/src/utils/axiosIntercept.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_BASE_URL } from './apiService';
+// import { API_BASE_URL } from './apiService';
 
 const ACCESS_TOKEN = window.localStorage.getItem('accessToken');
 const netaxios = axios.create({
