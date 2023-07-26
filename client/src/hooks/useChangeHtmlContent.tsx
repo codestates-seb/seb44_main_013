d28dec786732c07e7b320f0405c05785c2ccebd1
Kim-DaHam <times7749@naver.com>
2023-07-26 18:22:54 +0900

    Fix: htmlContent 안 쌍따옴표를 따옴표로 변경한다.

--- client/src/hooks/useChangeHtmlContent.tsx
+++ client/src/hooks/useChangeHtmlContent.tsx
@@ -23,6 +23,7 @@ export default function useChangeHtmlContent() {
     let copiedHtmlContent = htmlContent;
     copiedHtmlContent = copiedHtmlContent.replace(/<img/g, '<img style="width:100%; height:auto;"');
     copiedHtmlContent = copiedHtmlContent.replace(/<iframe/g, '<iframe style="width:100%;" height="696"');
+    copiedHtmlContent = copiedHtmlContent.replace(/"/g, "'");
     return copiedHtmlContent;
   }
 
