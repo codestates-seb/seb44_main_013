7a0b3fd26080a168a74ea63a118acd7079d21c7e
Kim-DaHam <times7749@naver.com>
2023-07-26 14:48:06 +0900

    Fix: ALLOWED_TAGS를 추가한다.

--- client/src/hooks/useChangeHtmlContent.tsx
+++ client/src/hooks/useChangeHtmlContent.tsx
@@ -1,13 +1,17 @@
 /* 2023-07-22 htmlContent 전환(?) 훅 - 김다함 */
 import dompurify from "dompurify";
 
+const ALLOWED_TAGS = ["iframe", "img", "br", "u", "link", "span", "p", "h1", "h2", "h3", "h4", "h5", "h6"];
 
 export default function useChangeHtmlContent() {
   const sanitizer = dompurify.sanitize;
 
   const sanitize = (htmlContent: string) => {
     return sanitizer(htmlContent,
-      { ALLOWED_TAGS: ["iframe", "img"], ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling'] })
+      {
+        ALLOWED_TAGS: ALLOWED_TAGS,
+        ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling']
+      })
   };
 
   const setElementInlineStyle = (htmlContent: string) => {
