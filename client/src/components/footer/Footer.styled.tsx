5469179e416ef0e948d213d027cf9796d0301dc3
HyoJeong-Park <parkhj3003@gmail.com>
2023-08-05 14:36:49 +0900

    fix: git 링크 추가

--- client/src/components/footer/Footer.styled.tsx
+++ client/src/components/footer/Footer.styled.tsx
@@ -1,15 +1,34 @@
 import tw from 'twin.macro';
+import styled from 'styled-components';
 
 export const FooterContainer = tw.div`
   w-screen 
   flex 
-  justify-center 
+  justify-between
   items-center 
   h-14
+  text-BASIC_WHITE 
+  text-[13px] 
+  font-thin
+  px-14
 `;
 
-export const FooterText = tw.div`
-  text-BASIC_WHITE 
-  text-[6px] 
-  font-thin
+// export const FooterText = tw.div`
+//   // text-BASIC_WHITE 
+//   // text-[6px] 
+//   // font-thin
+// `;
+
+
+export const FooterGithub = styled.ul`
+  ${tw`
+    flex
+    items-center
+    justify-start
+  `}
+
+  >li {
+    padding: 0 15px 0 0;
+  }
+  
 `;
--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,9 +1,24 @@
-import { FooterContainer, FooterText } from './Footer.styled';
+import { FooterContainer, FooterGithub } from './Footer.styled';
 
 export default function Footer() {
   return (
     <FooterContainer>
-      <FooterText>Copyright ⓒ 2023. Portfolly all rights reserved.</FooterText>
+      <p>Copyright ⓒ  2023. Portfolly all rights reserved.</p>
+      <div>
+          <FooterGithub>
+            <li>[FE]</li>
+            <li><a href='https://github.com/Kim-DaHam'>김다함</a></li>
+            <li><a href='https://github.com/HyoJeong-Park'>박효정</a></li>
+            <li><a href='https://github.com/yeon0226'>위정연</a></li>
+            <li><a href='https://github.com/Emma-Hyejin'>조혜진</a></li>
+          </FooterGithub>
+          <FooterGithub>
+            <li>[BE]</li>
+            <li><a href='https://github.com/sstar914'>김샛별</a></li>
+            <li><a href='https://github.com/dlglwo123'>이희재</a></li>
+            <li><a href='https://github.com/sftm0715'>정다희</a></li>
+          </FooterGithub>
+        </div>
     </FooterContainer>
   );
 }
