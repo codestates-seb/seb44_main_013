3f4a895cd6b2d41f775ccbd9805517c922a70525
HyoJeong-Park <parkhj3003@gmail.com>
2023-08-22 20:28:37 +0900

    fix: 제목 내용 수정

--- client/src/components/home-section/portfolio-section/PortfolioSection.tsx
+++ client/src/components/home-section/portfolio-section/PortfolioSection.tsx
@@ -13,7 +13,7 @@ export default function PortfolioSection() {
   return (
     <SectionWrapper>
       <PortfolioContents>
-        <h1>포트폴리에서 <br /> 여러분의 작품을 보여주세요.</h1>
+        <h1>포트폴리에서 <br /> 여러분의 작품을 보여주세요!</h1>
         <p>웹, 앱, 3D, 그래픽, 사진 카테고리에 자유롭게 <br /> 여러분만의 작품을 공유하고 전시해보세요.</p>
         <div><PurpleBtn>포트폴리오 보러가기</PurpleBtn></div>
       </PortfolioContents>
